9b3558d501808c1da19b799cf88062d5145d7a59
dharitritilak <67179700+dharitritilak@users.noreply.github.com>
2020-07-01 20:52:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Global Variables
+var banana, banana_img;
+var jungle, jungle_img;
+var stone, stone_img;
+
+
+
+var ground, ground_img;
+
+var restart, gameOver, restart_Img, gameOver_Img;
+
+function preload(){
+  
+}
+
+
+function setup() {
+  createCanvas(600,300);
+}
+
+
+function draw(){
+ background(255); 
+}
